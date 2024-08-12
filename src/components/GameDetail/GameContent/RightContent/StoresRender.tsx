e84ca083f40122387204b735139a6c2ea2f547f4
Evgeni Moskalev <MoskalevE12@yandex.ru>
2024-08-13 00:34:18 +0300

    almost done w stores render

--- src/components/GameDetail/GameContent/RightContent/StoresRender.tsx
+++ src/components/GameDetail/GameContent/RightContent/StoresRender.tsx
@@ -1,3 +1,4 @@
+import { platformIcons } from '../../../../icons/getPlatformIcon'
 import { TStores } from '../../../../store/detail/detail-types'
 
 interface Props {
@@ -5,5 +6,37 @@ interface Props {
 }
 
 export const StoresRender = ({ stores }: Props) => {
-  return <div className="game-stores-con"></div>
+  return (
+    <div className="game-stores-con">
+      <h3 className="game-stores-con--title">Where to buy</h3>
+      <div className="game-stores">
+        {stores?.map((store) => {
+          // сделать кнопкой див, в диве будет название магазина и
+          // иконка
+          // сделать отдельный компонент
+          const iconSrc = platformIcons[store.store.name]
+          return (
+            <>
+              <a
+                className="game-store-btn"
+                key={store.id}
+                href={`https://${store.store.domain}`}
+                target="_blank"
+              >
+                {store.store.name}
+              </a>
+              <img
+                src={iconSrc}
+                style={{
+                  width: 24,
+                  height: 24,
+                  marginRight: 8,
+                }}
+              />
+            </>
+          )
+        })}
+      </div>
+    </div>
+  )
 }
