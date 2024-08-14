bc0e97d26c9f160be1b7baa74078450a29e15c06
Evgeni Moskalev <MoskalevE12@yandex.ru>
2024-08-14 22:10:59 +0300

    almost done (?)

--- src/hooks/useGamesList.ts
+++ src/hooks/useGamesList.ts
@@ -2,10 +2,17 @@ import { useSelector } from 'react-redux'
 import { useEffect } from 'react'
 
 import { RootState } from '../store/rootReducer'
-import { loadGames } from '../store/all-games-list/all-games-actions'
-import { setParams } from '../store/params/params-actions'
+import {
+  clearGames,
+  loadGames,
+} from '../store/all-games-list/all-games-actions'
+import {
+  resetParams,
+  setParams,
+} from '../store/params/params-actions'
 
 import { useAppDispatch } from '../types'
+import { useLocation } from 'react-router-dom'
 
 export const useGamesList = () => {
   const dispatch = useAppDispatch()
@@ -14,16 +21,21 @@ export const useGamesList = () => {
   )
   const params = useSelector((state: RootState) => state.params)
   const pageCount = params.page as number
+  const location = useLocation()
 
   useEffect(() => {
     console.log('load games effect runs')
     dispatch(loadGames(params))
-
-    // return () => {
-    //   dispatch(clearGames())
-    //   dispatch(resetParams())
-    // }
   }, [dispatch, params])
+
+  useEffect(() => {
+    // для очистки массива игр и параметров запроса
+    // при размонтировании страницы
+    return () => {
+      dispatch(clearGames())
+      dispatch(resetParams())
+    }
+  }, [dispatch, location.pathname])
 
   const loadNextPage = () => {
     dispatch(setParams({ page: pageCount + 1 }))
--- src/pages/NewReleases/LastMonthGames.tsx
+++ src/pages/NewReleases/LastMonthGames.tsx
@@ -1,24 +1,41 @@
 import { useGamesList } from '../../hooks/useGamesList'
-import { useFilterParams } from '../../hooks/useFilterParams'
 
 import { Filters } from '../../components/Filters'
 import { useRangeDays } from '../../hooks/useRangeDays'
 import { Loading } from '../../components/Games/Loading'
 import { Games } from '../../components/Games/Games'
+import { PagButtons } from '../../components/Games/pagButtons/PagButtons'
 
 export const LastMonthGames = () => {
-  const { error, status, games } = useGamesList()
   const date = useRangeDays(-30)
-  // useFilterParams(date)
+  const {
+    error,
+    status,
+    games,
+    loadNextPage,
+    loadPrevPage,
+    pageCount,
+  } = useGamesList()
+
+  // при загрузке основной страницы почему-то криво заполняется
+  // массив игр для отображения
+  // разобраться (как-то)
 
   return (
     <div className="games-n-filters">
-      <Filters order platforms />
       <>
+        <Filters order platforms />
         {status === 'loading' && <Loading />}
         {error && <h1>Error: {error}</h1>}
         {status === 'received' && (
-          <Games title="Last Month" games={games} />
+          <>
+            <Games title="All Games" games={games} />
+            <PagButtons
+              pageCount={pageCount}
+              loadPrevPage={loadPrevPage}
+              loadNextPage={loadNextPage}
+            />
+          </>
         )}
       </>
     </div>
