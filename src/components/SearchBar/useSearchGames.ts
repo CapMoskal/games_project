b223308e0f290b4d809c3aa3b113d51dbac71839
Evgeni Moskalev <MoskalevE12@yandex.ru>
2024-08-27 13:11:09 +0400

    removed debounce on searchbar

--- src/components/SearchBar/useSearchGames.ts
+++ src/components/SearchBar/useSearchGames.ts
@@ -3,44 +3,25 @@ import { Game } from '../../types'
 import { API_KEY } from '../../config'
 
 export const useSearchGames = (
-  debounceTimeout: NodeJS.Timeout | null,
   query: string,
   setSuggestions: React.Dispatch<React.SetStateAction<Game[]>>,
-  setIsDropdownVisible: React.Dispatch<React.SetStateAction<boolean>>,
-  setDebounceTimeout: React.Dispatch<
-    React.SetStateAction<NodeJS.Timeout | null>
-  >
+  setIsDropdownVisible: React.Dispatch<React.SetStateAction<boolean>>
 ): void => {
   useEffect(() => {
-    if (debounceTimeout) {
-      clearTimeout(debounceTimeout)
-    }
-
-    if (query.length > 2) {
-      const timeoutId = setTimeout(() => {
-        fetch(
-          `https://api.rawg.io/api/games?key=${API_KEY}&search=${query}`
+    if (query.length > 0) {
+      fetch(
+        `https://api.rawg.io/api/games?key=${API_KEY}&search=${query}`
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          setSuggestions(data.results)
+          setIsDropdownVisible(true)
+        })
+        .catch((error) =>
+          console.error('Error fetching data:', error)
         )
-          .then((response) => response.json())
-          .then((data) => {
-            setSuggestions(data.results)
-            setIsDropdownVisible(true)
-          })
-          .catch((error) =>
-            console.error('Error fetching data:', error)
-          )
-      }, 500)
-
-      setDebounceTimeout(timeoutId)
     } else {
-      setSuggestions([])
       setIsDropdownVisible(false)
-    }
-
-    return () => {
-      if (debounceTimeout) {
-        clearTimeout(debounceTimeout)
-      }
     }
   }, [query])
 }
